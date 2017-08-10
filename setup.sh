80ad19b324920ce32b4065f2cb79b199e15b0787
fd3kyt <fd3kyt@gmail.com>
2017-08-10 09:41:19 +0800

    don't install oh-my-zsh in setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,9 @@ set -e
 set -x
 cd ~
 
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# install oh-my-zsh
+# sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
 git clone https://github.com/fd3kyt/.zsh_custom.git
 mv ~/.zshrc ~/.zshrc.backup
 cp ~/.zsh_custom/.zshrc.template ~/.zshrc
