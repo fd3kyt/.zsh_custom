528a24103a9da366e640730582e092420e0d4dd2
fd3kyt <fd3kyt@gmail.com>
2017-08-16 17:08:16 +0800

    add update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-
+cd "$ZSH_CUSTOM" || exit
+fetch_output=$(git fetch --dry-run)
+if [[ -n $fetch_output ]];then
+    git pull
+    zsh
+else
+    echo "Already up to date."
+fi
