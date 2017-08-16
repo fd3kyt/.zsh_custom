2a5ec27addf4aa94dbb9aeca05d5beba7abd125e
fd3kyt <fd3kyt@gmail.com>
2017-08-16 17:08:22 +0800

    clone_to_sources: cd to the new dir

--- clone_to_sources.zsh
+++ clone_to_sources.zsh
@@ -1,4 +1,8 @@
 function clone_to_sources(){
     cd ~/Sources/ || return 1
-    git clone $*
+    # By default, if stderr is not attached to a terminal, the clone
+    # progress is not shown. Use --progress to force it.
+    dir_string=$(git clone --progress $* 2>&1 | tee /dev/tty | grep -P "^Cloning into")
+    dir_name=$(echo "$dir_string" | grep -P -o "'\w+'" | grep -P -o "\w+")
+    cd $dir_name || return 1
 }
