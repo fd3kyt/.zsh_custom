5aeb2ff1ac50256a63e7311869835b52bc609512
fd3kyt <fd3kyt@gmail.com>
2017-12-18 14:23:55 +0800

    clone_to_source fix regexp

--- clone_to_sources.zsh
+++ clone_to_sources.zsh
@@ -3,6 +3,6 @@ function clone_to_sources(){
     # By default, if stderr is not attached to a terminal, the clone
     # progress is not shown. Use --progress to force it.
     dir_string=$(git clone --progress $* 2>&1 | tee /dev/tty | grep -P "^Cloning into")
-    dir_name=$(echo "$dir_string" | grep -P -o "'\w+'" | grep -P -o "\w+")
-    cd $dir_name || return 1
+    dir_name=$(echo "$dir_string" | grep -P -o "'[A-Za-z_-]+'" | grep -P -o "[A-Za-z_-]+")
+    cd "$dir_name" || return 1
 }
