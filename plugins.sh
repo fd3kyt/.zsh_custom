5432fec72afed36494e3e5e391c0d72beef56aa1
fd3kyt <fd3kyt@gmail.com>
2017-12-18 14:24:17 +0800

    add comment

--- plugins.sh
+++ plugins.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# It seems that this file has to be .sh instead of .zsh.
+
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
