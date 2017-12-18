ed055093799a91c4f13a7b683b0d11c824142c37
fd3kyt <fd3kyt@gmail.com>
2017-12-18 14:24:24 +0800

    move ~/Projects/goldfish/scripts/goldfish.zsh here

--- goldfish.zsh
+++ goldfish.zsh
@@ -1,1 +1,6 @@
-source "/home/fd3kyt/Projects/goldfish/scripts/goldfish.zsh"
+function gf-sshfs(){
+    set -x
+    sshfs "kyt@192.168.1.74:" "/home/74/"
+    sshfs "goldfish@192.168.1.74:" "/home/74.goldfish/"
+    sshfs "root@192.168.1.45:/" "/home/45.root-dir/"
+}
