5ed934334d088b2a0fbcab98754c71de15cfe5a2
fd3kyt <fd3kyt@gmail.com>
2018-01-02 10:29:15 +0800

    move goldfish.zsh to goldfish_initialize

--- goldfish.zsh
+++
@@ -1,6 +0,0 @@
-function gf-sshfs(){
-    set -x
-    sshfs "kyt@192.168.1.74:" "/home/74/"
-    sshfs "goldfish@192.168.1.74:" "/home/74.goldfish/"
-    sshfs "root@192.168.1.45:/" "/home/45.root-dir/"
-}
